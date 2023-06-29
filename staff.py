2caebeb5dadba9843c7eda5431541be989d1f556
Andrea Gussoni <andreagus@outlook.it>
2023-06-29 11:04:20 +0200

    staff added

---
+++ staff.py
@@ -0,0 +1,12 @@
+class Staff:
+
+    @staticmethod
+    def add_staff(db_connection_cursor, pj_name, designation, rate):
+        command = f'INSERT INTO staff_{pj_name} (designation, rate) VALUES (?, ?)'
+        db_connection_cursor.execute(command, (designation, rate))
+        db_connection_cursor.connection.commit()
+
+    @staticmethod
+    def update_staff(db_connection, df, pj_name):
+        
+        df.to_sql(f'staff_{pj_name}', db_connection, if_exists='replace', index=False)
