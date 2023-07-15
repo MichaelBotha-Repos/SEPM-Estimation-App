ed6a127b55a8a88f93e5f5f9e219ca394bf29d1d
mw509 <moonwalker509@gmail.com>
2023-07-15 23:01:19 +0200

    unit tests
    added unit tests

--- tests/task_test.py
+++ tests/task_test.py
@@ -1,61 +1,112 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest
 from unittest.mock import patch
-from io import StringIO
-
 import pandas as pd
-
+import sqlite3
+from io import StringIO
 from task import Task
 
 
-class TaskTests(unittest.TestCase):
+class TestTask(unittest.TestCase):
+    def setUp(self):
+        self.task = Task()
+
+    def tearDown(self):
+        pass
+
     def test_add_description(self):
-        task = Task()
-        with patch('builtins.input', return_value='Test Description'):
-            task.add_description()
-        self.assertEqual(task.task_description, 'Test Description')
+        with patch('builtins.input', return_value='Task description'):
+            self.task.add_description()
+        self.assertEqual(self.task.task_description, 'Task description')
 
     def test_add_estimations(self):
-        task = Task()
         with patch('builtins.input', side_effect=['1', '2', '3']):
-            task.add_estimations()
-        self.assertEqual(task.task_estimations, [1, 2, 3])
+            self.task.add_estimations()
+        self.assertEqual(self.task.task_estimations, [1, 2, 3])
 
     def test_add_chosen_estimation(self):
-        task = Task()
         with patch('builtins.input', return_value='2'):
-            task.add_chosen_estimation()
-        self.assertEqual(task.chosen_estimation, 2)
+            self.task.add_chosen_estimation()
+        self.assertEqual(self.task.chosen_estimation, 2)
 
-    @patch('sqlite3.Cursor')
-    def test_add_task(self, mock_cursor):
-        task = Task('Test Description', [1, 2, 3], 2)
-        Task.add_task(mock_cursor, 'project_name', task.task_description, task.task_estimations, task.chosen_estimation,
-                      'staff_name')
-        mock_cursor.execute.assert_called_once_with(
-            'INSERT INTO tasks_project_name (task_description, estimate1, estimate2, estimate3, chosen_estimate, '
-            'allocated_staff) VALUES (?, ?, ?, ?, ?, ?)',
-            ('Test Description', 1, 2, 3, 2, 'staff_name')
-        )
+    def test_add_task(self):
+        # Connect to an in-memory SQLite database
+        conn = sqlite3.connect(':memory:')
+        cursor = conn.cursor()
+        project_name = 'TestProject'
+        desc = 'Task description'
+        est_1, est_2, est_3 = 1, 2, 3
+        chosen_est = 2
+        staff = 'John Doe'
 
-    @patch('pandas.DataFrame.to_sql')
-    def test_update_tasks(self, mock_to_sql):
-        task = Task('Test Description', [1, 2, 3], 2)
-        df = pd.DataFrame({'task_id': [1, 2, 3]})
-        Task.update_tasks('db_connection', df, 'project_name')
-        mock_to_sql.assert_called_once_with(
-            'tasks_project_name',
-            'db_connection',
-            if_exists='replace',
-            index=False,
-            dtype={'task_id': 'INTEGER PRIMARY KEY AUTOINCREMENT'}
-        )
+        Task.add_task(cursor, project_name, desc, est_1, est_2, est_3, chosen_est, staff)
 
-    @patch('sqlite3.Cursor')
-    def test_delete_task(self, mock_cursor):
-        Task.delete_task(mock_cursor, 'project_name', 123)
-        mock_cursor.execute.assert_called_once_with(
-            'DELETE FROM tasks_project_name WHERE task_id == 123'
-        )
+        # Retrieve the inserted task from the database
+        cursor.execute(f"SELECT * FROM tasks_{project_name}")
+        result = cursor.fetchall()
+
+        expected_result = [(1, desc, est_1, est_2, est_3, chosen_est, staff)]
+        self.assertEqual(result, expected_result)
+
+        # Clean up
+        conn.close()
+
+    def test_update_tasks(self):
+        # Connect to an in-memory SQLite database
+        conn = sqlite3.connect(':memory:')
+        cursor = conn.cursor()
+        project_name = 'TestProject'
+
+        # Create a sample DataFrame
+        data = {
+            'task_id': [1, 2, 3],
+            'task_description': ['Task 1', 'Task 2', 'Task 3'],
+            'estimate1': [1, 2, 3],
+            'estimate2': [2, 4, 6],
+            'estimate3': [3, 6, 9],
+            'chosen_estimate': [2, 4, 6],
+            'allocated_staff': ['John', 'Jane', 'Bob']
+        }
+        df = pd.DataFrame(data)
+
+        Task.update_tasks(cursor, df, project_name)
+
+        # Retrieve the inserted tasks from the database
+        cursor.execute(f"SELECT * FROM tasks_{project_name}")
+        result = cursor.fetchall()
+
+        expected_result = [(1, 'Task 1', 1, 2, 3, 2, 'John'),
+                           (2, 'Task 2', 2, 4, 6, 4, 'Jane'),
+                           (3, 'Task 3', 3, 6, 9, 6, 'Bob')]
+        self.assertEqual(result, expected_result)
+
+        # Clean up
+        conn.close()
+
+    def test_delete_task(self):
+        # Connect to an in-memory SQLite database
+        conn = sqlite3.connect(':memory:')
+        cursor = conn.cursor()
+        project_name = 'TestProject'
+        task_id = 1
+
+        # Insert a sample task into the database
+        cursor.execute(f"INSERT INTO tasks_{project_name} (task_id) VALUES (?)", (task_id,))
+        conn.commit()
+
+        Task.delete_task(cursor, project_name, task_id)
+
+        # Retrieve the remaining tasks from the database
+        cursor.execute(f"SELECT * FROM tasks_{project_name}")
+        result = cursor.fetchall()
+
+        expected_result = []
+        self.assertEqual(result, expected_result)
+
+        # Clean up
+        conn.close()
 
 
 if __name__ == '__main__':
